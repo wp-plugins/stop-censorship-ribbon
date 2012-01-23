0446bffb4d32b6c4f9daceac2670d1b543584783
mindshare <mindshare@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-23 22:03:31 +0000

    git-svn-id: https://plugins.svn.wordpress.org/stop-censorship-ribbon/tags/1.0.3@494257 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ stop-censorship-ribbon.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: Stop Censorship Ribbon
+Plugin URI: http://mind.sh/are/downloads/stop-censorship-wordpress-plugin/
+Description: When activated, this plugin will put a Stop Censorship ribbon on the top right corner of your website. This is a fork of Konstantin Kovshenin's plugin.
+Author: Mindshare Studios
+Version: 1.0.3
+License: GPLv2
+Author URI: http://mind.sh/are/
+*/
+
+function render_stop_censorship_ribbon() {
+	$ribbon_url = plugins_url('stop-censorship-ribbon.png', __FILE__ );
+	if(function_exists('is_admin_bar_showing')) {
+		$padding_top = is_admin_bar_showing() ? 28 : 0;
+	} else {
+		$padding_top = 0;
+	}
+	echo "<a target='_blank' class='stop-censorship-ribbon' href='http://americancensorship.org/'><img src='{$ribbon_url}' alt='Stop censorship' style='position: fixed; top: ".$padding_top ."px; right: 0; z-index: 100000; cursor: pointer; border:none;' /></a>";
+}
+add_action('wp_footer', 'render_stop_censorship_ribbon');
