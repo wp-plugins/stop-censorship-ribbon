45571d18db49c31710885a8614470a5a81a8bbb0
mindshare <mindshare@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-02 16:27:49 +0000

    updated link to http://act.freepress.net/sign/internetdeclaration/
    git-svn-id: https://plugins.svn.wordpress.org/stop-censorship-ribbon/trunk@566517 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- stop-censorship-ribbon.php
+++ stop-censorship-ribbon.php
@@ -4,7 +4,7 @@ Plugin Name: Stop Censorship Ribbon
 Plugin URI: http://mind.sh/are/downloads/stop-censorship-wordpress-plugin/
 Description: When activated, this plugin will put a Stop Censorship ribbon on the top right corner of your website.
 Author: Mindshare Studios
-Version: 1.0.4
+Version: 1.0.5
 License: GPLv2
 Author URI: http://mind.sh/are/
 */
@@ -16,6 +16,6 @@ function render_stop_censorship_ribbon() {
 	} else {
 		$padding_top = 0;
 	}
-	echo "<a target='_blank' class='stop-censorship-ribbon' href='http://cyberspying.eff.org/'><img src='{$ribbon_url}' alt='Stop censorship' style='position: fixed; top: ".$padding_top ."px; right: 0; z-index: 100000; cursor: pointer; border:none;' /></a>";
+	echo "<a target='_blank' class='stop-censorship-ribbon' href='http://act.freepress.net/sign/internetdeclaration/' title='Join the fight for internet freedom'><img src='{$ribbon_url}' alt='Stop censorship' style='position: fixed; top: ".$padding_top ."px; right: 0; z-index: 100000; cursor: pointer; border:none;' /></a>";
 }
 add_action('wp_footer', 'render_stop_censorship_ribbon');
